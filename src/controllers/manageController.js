dddf92f09decc29c2e47f01d75b0b86b98ce3579
RikiKawasaki <riki1027_one@i.softbank.jp>
2020-12-30 19:33:30 +0900

    second commit

--- src/controllers/manageController.js
+++ src/controllers/manageController.js
@@ -1,36 +1,69 @@
 const db = require("../models");
 
 const manageControl = {
-    showAllClient: async function(req, res, next) {
+    home: (req, res, next) => {
+        res.render("home", { title: "顧客管理ツール" });
+    },
+
+    showAllClient: async (req, res, next) => {
         const manages = await db.Manage.findAll();
         res.render('allClients', { title: '顧客一覧', manages });
     },
 
-    setClient: function(req, res, next) {
+    create: (req, res, next) => {
+        res.render("create", { title: "新規登録" });
+    },
+
+    confirm: (req, res, next) => {
         const obj = req.body;
-        db.Manage.create({
+        res.render("confirm", {
+            title: "確認",
             clientName: obj.clientName,
             address: obj.address,
             tel: obj.tel
         });
-        res.redirect("/");
     },
 
-    create: function(req, res, next) {
-        res.render("create", { title: "新規登録" });
-    },
-
-    confirm: function(req, res, next) {
-        const clientName = req.body.clientName;
-        const address = req.body.address;
-        const tel = req.body.tel;
-        res.render("confirm", {
-            title: "確認",
-            clientName: clientName,
-            address: address,
-            tel: tel
+    setClient: (req, res, next) => {
+        db.Manage.create({
+            clientName: req.body.clientName,
+            address: req.body.address,
+            tel: req.body.tel
         });
+        res.redirect("/showAllClient");
     },
+
+    edit: async (req, res, next) => {
+        const manages = await db.Manage.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.render("edit", { title: "編集", manages });
+    },
+
+    update: (req, res, next) => {
+        db.Manage.update({
+            clientName: req.body.clientName,
+            address: req.body.address,
+            tel: req.body.tel
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.redirect("/showAllClient");
+    },
+
+    delete: async (req, res, next) => {
+        const id = await req.params.id;
+        db.Manage.destroy({
+            where: {
+                id: id
+            }
+        });
+        res.redirect("/showAllClient");
+    }
 };
 
 module.exports = manageControl;
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const manageControl = require("../controllers/manageController");
 
-router.get("/", manageControl.showAllClient);
-router.post("/", manageControl.setClient);
+router.get("/", manageControl.home);
+router.get("/showAllClient", manageControl.showAllClient);
 router.get("/create", manageControl.create);
 router.post("/confirm", manageControl.confirm);
+router.post("/", manageControl.setClient);
+router.get("/edit/:id", manageControl.edit);
+router.post("/update/:id", manageControl.update);
+router.get("/delete/:id", manageControl.delete);
 
 module.exports = router;
